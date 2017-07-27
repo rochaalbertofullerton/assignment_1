882f090ca22313e82f90fcde6946bf3b24b012c5
rochaalbertofullerton <berto323@csu.fullerton.edu>
2017-07-26 18:59:07 -0700

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -24,24 +24,20 @@ using namespace std;
 
 
         string cmdBuff;          // comment buffer
-
-
         pid_t pid;              // ID of child process
 
         do{
 
-                /* Prompt the user to enter the command */
-
+               // ask the user for the command and store it in cmdBuff
                 cerr << "cmd>";
                 cin >> cmdBuff;
-
-                /* If the user wants to exit */
-
+         
+                // check if the user wants to continue or exit the program
                 if(cmdBuff != "exit"){
 
-                        /* TODO: Create a child */
                         pid = fork();
 
+                        
                         if(pid <  0){
 
                                 cout << "child could not fork()" << endl;
@@ -50,7 +46,7 @@ using namespace std;
                         }
                         else if(pid == 0){
 
-
+                                // check if the command the user entered was found or valid
                                 if(execlp(cmdBuff.c_str(), cmdBuff.c_str() , NULL)== -1){
 
                                         cout << " cmd not found" << endl;
